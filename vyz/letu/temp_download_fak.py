dbaf417a4c00c350fbb9d08d182cbf2479aac776
vysheslav <109220857+vysheslav@users.noreply.github.com>
2022-07-13 23:18:02 +0500

    Add files via upload

---
+++ vyz/letu/temp_download_fak.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def download():
+    with open('out.html','w',encoding='utf-8') as out:
+        url = 'https://abit.etu.ru/ru/postupayushhim/bakalavriat-i-specialitet/spiski-podavshih-zayavlenie/'
+        r = requests.get(url)
+        out.write(r.text)
+#download()
+def pars():
+    with open('out.html','r',encoding='utf-8') as html:        
+        soup = BeautifulSoup(html, "html.parser")
+        for i in soup.findAll('tr'):
+            print(i)
+            input()
+pars()
