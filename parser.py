05db8995daa6ff76a4c740b10e2bcba344ec37ed
vysheslav <109220857+vysheslav@users.noreply.github.com>
2022-07-15 01:25:43 +0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,12 +1,11 @@
 import os
 
+if 'out_json' not in os.listdir():
+    os.mkdir('./out_json')
+    
 for folder in os.listdir('./vyz'):
     path = './vyz/' + folder# + '/' + folder + '_parser.py'
     os.chdir(path)   
     os.system('cmd /c python ' + folder + '_parser.py')
     #os. _exit()
     os.chdir('..'); os.chdir('..')
-
-    
-    
-    
