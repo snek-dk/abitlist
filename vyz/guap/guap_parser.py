b85f2a1cc21f16ed9b1471555e4b152a254c60a7
Stivux <34604045+Stivux@users.noreply.github.com>
2022-07-23 00:06:42 +0400

    Update guap_parser.py

--- vyz/guap/guap_parser.py
+++ vyz/guap/guap_parser.py
@@ -2,52 +2,49 @@ import http.client
 import json
 import re
 from lxml import html
-CONN = http.client.HTTPSConnection('priem.guap.ru')
-TERMS = {'14': 'ОК', '15': 'ОК', '16': 'ЦП', '20': 'ОП', '25': 'СК', '50': 'ИН'}
-content = ''
-special = {}
-cols, data = [], []
-
-CONN.request('GET', '/_lists/Pred_37')
-content = CONN.getresponse().read().decode('utf-8')
-parsed = html.fromstring(content)
-for row in parsed.findall('.//*/table/tbody/tr'):
-    cols = row.findall('td')
-    special[f'{cols[0].text} {cols[1].text}'] = [a.get('href') for a in row.findall('td/a')]
-
-for spec in special:
-    for url in special[spec]:
-        CONN.request('GET', url)
-        content = CONN.getresponse().read().decode('utf-8')
+S = http.client.HTTPSConnection('priem.guap.ru')
+SUFF = {'26': 'Заочная', '27': 'Заочная', '29': 'Очно-заочная', '36': 'Очная', '37': 'Очная'}
+TERMS = {'14': 'ОК', '15': 'К', '16': 'ЦП', '20': 'ОП', '25': 'СК', '50': 'ИН'}
+cols, data, ege = [], [], []
+content, spec, url, conc = '', '', '', ''
+for suff in SUFF:
+    S.request('GET', f'/_lists/Pred_{suff}')
+    content = S.getresponse().read()
+    parsed = html.fromstring(content)
+    for a in parsed.findall('.//table/tbody/tr/td/a'):
+        url = a.get('href')
+        S.request('GET', url)
+        content = S.getresponse().read()
         parsed = html.fromstring(content)
-        for row in parsed.findall('.//*/table/tbody/tr'):
-            cols = [i.text_content() for i in row.findall('td')]
+        spec = parsed.findall('.//h3')[1].text
+        for row in parsed.findall('.//table/tbody/tr'):
+            cols = [i.text for i in row.findall('td')]
             conc = TERMS[url[-2:]]
             if cols[4] == 'Без В/И':
-                cols[4] = "0"
-                cols[2] = "0"
                 conc = 'БВИ'
-            ege = re.findall(r"(\d+)", cols[1], flags=re.M)
-            ege += ['0'] * (3 - len(ege))
+                cols[2] = cols[4] = "0"
+            if cols[1]:
+                ege = re.findall(r"(\d+)", cols[1])
+            ege += ["0"] * (3-len(ege))
             data.append({
-            'ВУЗ': 'ГУАП',
-            'Направление': spec,
-            'ОП': spec,
-            'Форма_обучения': 'Очная',
-            'Основа_обучения': 'Госбюджет' if url[-2:] != '15' else 'Контракт',
-            'СНИЛС_УК': cols[0] if cols[0] else None,
-            'Конкурс': conc,
-            'СУММА': cols[4] if cols[4] else None,
-            'СУММА_БЕЗ_ИД': cols[2] if cols[2] else None,
-            'ВИ_1': ege[0] if ege[0] else None,
-            'ВИ_2': ege[1] if ege[1] else None,
-            'ВИ_3': ege[2] if ege[2] else None,
-            'ВИ_4': None,
-            'ВИ_5': None,
-            'ИД': cols[3] if cols[3] else None,
-            'Согласие': cols[6],
-            'Оригинал': cols[7]
-        })
-CONN.close()
+                'ВУЗ': 'ГУАП',
+                'Направление': spec,
+                'ОП': spec,
+                'Форма_обучения': SUFF[suff],
+                'Основа_обучения': 'Госбюджет' if url[-2:] != '15' else 'Контракт',
+                'СНИЛС_УК': cols[0] if cols[0] != '0' else None,
+                'Конкурс': conc,
+                'СУММА': cols[4],
+                'СУММА_БЕЗ_ИД': cols[2],
+                'ВИ_1': ege[0],
+                'ВИ_2': ege[1],
+                'ВИ_3': ege[2],
+                'ВИ_4': None,
+                'ВИ_5': None,
+                'ИД': cols[3],
+                'Согласие': cols[6],
+                'Оригинал': cols[7]
+            })
+S.close()
 with open('./out_json/guap.json', 'w', encoding='utf-8') as fp:
     json.dump(data, fp, indent=4, ensure_ascii=False)
