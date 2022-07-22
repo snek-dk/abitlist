f054806d313a0b3701d55b9512d354f183f467d4
vysheslav <vyshelav@local>
2022-07-22 09:41:48 +0500

    update, formating out json

--- vyz/letu/letu_parser.py
+++ vyz/letu/letu_parser.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 def letu_parser():
     #print('LETU')
-    json_dict = dict()
+    json_dict = list()
     sort_score = {
         "01.03.02" : 'рми',
         "09.03.01" : 'мри',
@@ -33,7 +33,6 @@ def letu_parser():
 #key_words = Автоматика
 
     k = 0
-
     with open('vyz/letu/source.json', 'r', encoding='utf-8') as input_file:
         input_json = json.load(input_file)
 
@@ -47,12 +46,12 @@ def letu_parser():
 
                 try:
                     r = requests.get(fak_url)
-                except:
-                    print(fak_url)
+                except:                  
                     time.sleep(0.2)
                     try:                   
                         r = requests.get(fak_url)
                     except:
+                        print(fak_url)
                         continue
                 soup = BeautifulSoup(r.text, "html.parser")
 
@@ -60,7 +59,7 @@ def letu_parser():
 
                     if len(teg) == 27:
                         data = teg.text.split('\n')[1:-1]
-                        json_dict[str(k)] = {
+                        json_dict.append({
                             'ВУЗ': 'ЛЭТИ',
                             'Направление': (fak_num + ' ' + fak_name).strip(),
                             'ОП': (fak_num + ' ' + fak_name).strip(),
@@ -78,25 +77,24 @@ def letu_parser():
                             'ИД': str(data[9]),
                             'Согласие': data[12],
                             'Оригинал': data[11]
-                        }
+                        })
                         
                         order_score = sort_score[fak_num]
 
                         if fak_num not in ("27.03.04", "42.03.01", "45.03.02"):
 
                             if order_score == 'рми':
-                                s1, s2, s3 = [json_dict[str(k)]['ВИ_' + str(j)] for j in range(1,4)]
-                                json_dict[str(k)]['ВИ_1'], json_dict[str(k)]['ВИ_2'], json_dict[str(k)]['ВИ_3'] = s2, s3, s1
+                                s1, s2, s3 = [json_dict[k]['ВИ_' + str(j)] for j in range(1,4)]
+                                json_dict[k]['ВИ_1'], json_dict[k]['ВИ_2'], json_dict[k]['ВИ_3'] = s2, s3, s1
 
                             elif order_score == 'мри':
-                                s1, s2, s3 = [json_dict[str(k)]['ВИ_' + str(j)] for j in range(1,4)]
-                                json_dict[str(k)]['ВИ_1'], json_dict[str(k)]['ВИ_2'], json_dict[str(k)]['ВИ_3'] = s1, s3, s2
+                                s1, s2, s3 = [json_dict[k]['ВИ_' + str(j)] for j in range(1,4)]
+                                json_dict[k]['ВИ_1'], json_dict[k]['ВИ_2'], json_dict[k]['ВИ_3'] = s1, s3, s2
 
                         elif fak_num == "27.03.04" and "Автоматика" not in fak_name:
 
-                            s1, s2, s3 = [json_dict[str(k)]['ВИ_' + str(j)] for j in range(1,4)]
-                            json_dict[str(k)]['ВИ_1'], json_dict[str(k)]['ВИ_2'], json_dict[str(k)]['ВИ_3'] = s1, s3, s2
-
+                            s1, s2, s3 = [json_dict[k]['ВИ_' + str(j)] for j in range(1,4)]
+                            json_dict[k]['ВИ_1'], json_dict[k]['ВИ_2'], json_dict[k]['ВИ_3'] = s1, s3, s2
                         k += 1
 
     with open('./out_json/letu.json', 'w', encoding='utf-8') as out_file:
