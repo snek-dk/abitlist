fadcd08013d3a8ee1536bf5049a219a9bc30272f
snek <dmitrykhodosevich@yandex.ru>
2022-08-03 14:11:16 +0300

    update

--- vyz/mirea/mirea_parser.py
+++ vyz/mirea/mirea_parser.py
@@ -2,12 +2,12 @@ import os, subprocess
 import shutil
 
 cwd = os.getcwd()
-subprocess.call(['node', 'main.js'], cwd=cwd)
+# subprocess.call(['node', 'main.js'], cwd=cwd)
 
 for i in range(51):
     try:
-        if os.path.getsize(cwd + '\\' + f'МИРЭА{str(i)}.json')//(2**13) > 5:
-            shutil.move(cwd + '\\' + f'МИРЭА{str(i)}.json', '../../out_json')
+        if os.path.getsize(rf"{cwd}\МИРЭА{str(i)}.json")//(2**13) > 5:
+            shutil.move(rf"{cwd}\МИРЭА{str(i)}.json", '../../out_json')
     except:
         print("НЕ СПАРСИЛОСЬ :C, ПЕРЕЗАПУСТИ!!!")
 print('DONE\n')
