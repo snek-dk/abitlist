f952c9409d13a2bb2a49ff8863373c2202d95570
snek <dmitrykhodosevich@yandex.ru>
2023-07-06 18:06:15 +0300

    archive

---
+++ 2022/vyz/itmo/itmo_parser.py
@@ -0,0 +1,127 @@
+import json
+import time
+import requests
+import os
+
+
+def itmo_parser():
+    '''Parsing itmo site'''
+    # print('ITMO')
+    with open('vyz/itmo/url_id.json', 'r', encoding='utf-8') as input_id:
+        urls_id = json.load(input_id)['id_fak']
+    k = 0
+    json_dict = list()
+    name_qu = {'without_entry_tests': 'БВИ',
+               'by_unusual_quota': 'ОП',
+               'by_special_quota': 'СК',
+               'by_target_quota': 'ЦК',
+               'general_competition': 'ОК'}
+    name_o = {'budget': 'госбюджет',
+              'contract': 'контракт'}
+
+    def trans_snils(snils, case):
+        if snils != None:
+            return snils[:3] + '-' + snils[3:6] + '-' + snils[6:9] + ' ' + snils[9::]
+        return case
+
+    for url_id in urls_id:
+
+        urls = {'budget': 'https://abitlk.itmo.ru/api/v1/9e2eee80b266b31c8d65f1dd3992fa26eb8b4c118ca9633550889a8ff2cac429/rating/bachelor/' + 'budget' + '?program_id=' + url_id + '&manager_key=',
+                'contract': 'https://abitlk.itmo.ru/api/v1/9e2eee80b266b31c8d65f1dd3992fa26eb8b4c118ca9633550889a8ff2cac429/rating/bachelor/' + 'contract' + '?program_id=' + url_id + '&manager_key='}
+
+        for type_o, url in urls.items():
+            time.sleep(0.2)
+            head = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+            res = requests.get(url, headers=head, verify='./certificate/certs.pem').json()
+            try:
+                fakultet = res['result']['direction']['direction_title']
+            except:
+                print('ссылка битая')
+                continue
+            if len(res['result'].keys()) == 7:
+                # print(fakultet, type_o, k) ## debug!!!
+                for quota in name_qu.keys():
+                    for abbi in res['result'][quota]:
+
+                        json_dict.append({
+                            'ВУЗ': 'ИТМО',
+                            'Направление': fakultet,
+                            'ОП': fakultet,
+                            'Форма_обучения': 'очная',
+                            'Основа_обучения': name_o[type_o].strip(),
+                            'СНИЛС_УК': trans_snils(abbi['snils'], abbi['case_number']),
+                            'Конкурс': name_qu[quota].strip(),
+                            'СУММА': str(abbi['total_scores']) if abbi['total_scores'] != None else '0',
+                            'СУММА_БЕЗ_ИД': str(int(abbi['total_scores'] if abbi['total_scores'] != None else 0) - int(abbi['ia_scores'] if abbi['ia_scores'] != None else 0)),
+                            'ВИ_1': '0',
+                            'ВИ_2': '0',
+                            'ВИ_3': '0',
+                            'ВИ_4': None,
+                            'ВИ_5': None,
+                            'ИД': str(abbi['ia_scores']) if abbi['ia_scores'] != None else '0',
+                            'Согласие': 'ДА' if abbi['send_agreement'] else 'НЕТ',
+                            'Оригинал': 'ДА' if abbi['is_send_original'] else 'НЕТ'
+                        })
+
+                        if abbi['disciplines_scores'] != None and len(abbi['disciplines_scores']) > 0:
+                            for keys_disp, values_disp in abbi['disciplines_scores'].items():
+                                if 'матем' in str(keys_disp).lower():
+                                    json_dict[k]['ВИ_1'] = str(
+                                        values_disp)
+                                elif 'русс' in str(keys_disp).lower():
+                                    json_dict[k]['ВИ_3'] = str(
+                                        values_disp)
+                                else:
+                                    json_dict[k]['ВИ_2'] = str(
+                                        values_disp)
+                        for i in range(1, 4):
+                            if json_dict[k]['ВИ_'+str(i)] == "None":
+                                json_dict[k]['ВИ_'+str(i)] = "0"
+
+                        k += 1
+
+            else:
+
+                # res['result']['items']
+                for abbi in res['result']['items']:
+                    json_dict.append({
+                        'ВУЗ': 'ИТМО',
+                        'Направление': fakultet,
+                        'ОП': fakultet,
+                        'Форма_обучения': 'очная',
+                        'Основа_обучения': 'контракт',
+                        'СНИЛС_УК': trans_snils(abbi['snils'], abbi['case_number']),
+                        'Конкурс': name_qu[quota].strip(),
+                        'СУММА': str(abbi['total_scores']) if abbi['total_scores'] != None else '0',
+                        'СУММА_БЕЗ_ИД': str(int(abbi['total_scores'] if abbi['total_scores'] != None else 0) - int(abbi['ia_scores'] if abbi['ia_scores'] != None else '0')),
+                        'ВИ_1': '0',
+                        'ВИ_2': '0',
+                        'ВИ_3': '0',
+                        'ВИ_4': None,
+                        'ВИ_5': None,
+                        'ИД': str(abbi['ia_scores']) if abbi['ia_scores'] != None else '0',
+                        'Согласие': 'ДА' if abbi['send_agreement'] else 'НЕТ',
+                                    'Оригинал': 'ДА' if abbi['is_send_original'] else 'НЕТ'
+                    })
+
+                    if abbi['disciplines_scores'] != None and len(abbi['disciplines_scores']) > 0:
+                        for keys_disp, values_disp in abbi['disciplines_scores'].items():
+                            if 'матем' in str(keys_disp).lower():
+                                json_dict[k]['ВИ_1'] = str(values_disp)
+                            elif 'русс' in str(keys_disp).lower():
+                                json_dict[k]['ВИ_3'] = str(values_disp)
+                            else:
+                                json_dict[k]['ВИ_2'] = str(values_disp)
+
+                    for i in range(1, 4):
+                        if json_dict[k]['ВИ_'+str(i)] == "None":
+                            json_dict[k]['ВИ_'+str(i)] = "0"
+                    k += 1
+
+    with open('./out_json/itmo.json', 'w', encoding='utf-8') as out_file:
+        json.dump(json_dict, out_file, indent=4, ensure_ascii=False)
+    # print('DONE\n')
+
+
+itmo_parser()
---
+++ 2022/vyz/letu/letu_parser.py
@@ -0,0 +1,105 @@
+import os, requests, json, time
+from bs4 import BeautifulSoup
+
+def letu_parser():
+    #print('LETU')
+    json_dict = list()
+    sort_score = {
+        "01.03.02" : 'рми',
+        "09.03.01" : 'мри',
+        "09.03.02" : 'мри',
+        "09.03.04" : 'мри',
+        "10.05.01" : 'мри',
+        "11.03.01" : 'мри',
+        "11.03.02" : 'мри',
+        "11.03.03" : 'мри',
+        "11.03.04" : 'мри',
+        "11.05.01" : 'мри',
+        "12.03.01" : 'мри',
+        "12.03.04" : 'мри',
+        "13.03.02" : 'мир',
+        "15.03.06" : 'мри',
+        "20.03.01" : 'мри',
+        "27.03.02" : 'мри',
+        "27.03.03" : 'мри',
+        "27.03.04" : 'мри',
+        "27.03.05" : 'мри',
+        "28.03.01" : 'мри',
+        "42.03.01": '',
+        "45.03.02": ''
+    }
+
+#Управление в технических системах. Автоматика и робототехнические системы 27.03.04 мир
+#key_words = Автоматика
+
+    k = 0
+    with open('vyz/letu/source.json', 'r', encoding='utf-8') as input_file:
+        input_json = json.load(input_file)
+
+    for type_o in input_json.keys():
+
+        for faks_of_type_o in input_json[type_o].keys():
+
+            for fak in input_json[type_o][faks_of_type_o]:
+                #print(*list(map(lambda st: st.strip().replace('_', ' '), fak))[:2], k)
+                fak_num, fak_name, fak_url = map(lambda st: st.strip().replace('_', ' '), fak)
+                fak_url += '-competitive'
+                try:
+                    r = requests.get(fak_url)
+                except:                  
+                    time.sleep(0.2)
+                    try:                   
+                        r = requests.get(fak_url)
+                    except:
+                        print(fak_url)
+                        continue
+                soup = BeautifulSoup(r.text, "html.parser")
+
+                for teg in soup.findAll('tr'):
+
+                    if len(teg) == 27:
+                        data = teg.text.split('\n')[1:-1]
+                        json_dict.append({
+                            'ВУЗ': 'ЛЭТИ',
+                            'Направление': (fak_num + ' ' + fak_name).strip(),
+                            'ОП': (fak_num + ' ' + fak_name).strip(),
+                            'Форма_обучения': type_o,
+                            'Основа_обучения': faks_of_type_o,
+                            'СНИЛС_УК': data[1],
+                            'Конкурс': data[3],
+                            'СУММА': str(data[4]).strip() if len(str(data[4]).strip()) > 0 else "0",
+                            'СУММА_БЕЗ_ИД': str(data[5]).strip() if len(str(data[5]).strip()) > 0 else "0",
+                            'ВИ_1': str(data[6]).strip() if len(str(data[6]).strip()) > 0 else "0",
+                            'ВИ_2': str(data[7]).strip() if len(str(data[7]).strip()) > 0 else "0",
+                            'ВИ_3': str(data[8]).strip() if len(str(data[8]).strip()) > 0 else "0",
+                            'ВИ_4': None,
+                            'ВИ_5': None,
+                            'ИД': str(data[9]).strip() if len(str(data[9]).strip()) > 0 else "0",
+                            'Согласие': data[12],
+                            'Оригинал': data[11]
+                        })
+                        
+                        order_score = sort_score[fak_num]
+
+                        if fak_num not in ("27.03.04", "42.03.01", "45.03.02"):
+
+                            if order_score == 'рми':
+                                s1, s2, s3 = [json_dict[k]['ВИ_' + str(j)] for j in range(1,4)]
+                                json_dict[k]['ВИ_1'], json_dict[k]['ВИ_2'], json_dict[k]['ВИ_3'] = s2, s3, s1
+
+                            elif order_score == 'мри':
+                                s1, s2, s3 = [json_dict[k]['ВИ_' + str(j)] for j in range(1,4)]
+                                json_dict[k]['ВИ_1'], json_dict[k]['ВИ_2'], json_dict[k]['ВИ_3'] = s1, s3, s2
+
+                        elif fak_num == "27.03.04" and "Автоматика" not in fak_name:
+
+                            s1, s2, s3 = [json_dict[k]['ВИ_' + str(j)] for j in range(1,4)]
+                            json_dict[k]['ВИ_1'], json_dict[k]['ВИ_2'], json_dict[k]['ВИ_3'] = s1, s3, s2
+                        k += 1
+
+    with open('./out_json/letu.json', 'w', encoding='utf-8') as out_file:
+        json.dump(json_dict, out_file, indent=4, ensure_ascii=False)
+    #print('DONE\n')
+
+
+letu_parser()
